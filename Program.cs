ae4c2d6a6d66b26ed044818d5a55344e333c211d
Tiago Ramirez <tiagoramirez2001@gmail.com>
2022-10-21 15:51:43 -0300

    Added connection string

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using portfolio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<PortfolioContext>(builder.Configuration.GetConnectionString("cnPortfolio"));
 
 var app = builder.Build();
 
