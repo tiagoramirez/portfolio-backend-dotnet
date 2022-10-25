b1e1323b399d9c91932819b314c78b5fcc0ab784
Tiago Ramirez <tiagoramirez2001@gmail.com>
2022-10-25 00:17:38 -0300

    Finished with user controller and service

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,7 +5,7 @@ using portfolio.Services;
 namespace portfolio.Controllers;
 
 [ApiController]
-[Route("/api/[controller]")]
+[Route("/API/[controller]")]
 public class UserController : ControllerBase
 {
     IUserService _userService;
@@ -15,7 +15,7 @@ public class UserController : ControllerBase
         _userService = service;
     }
 
-    [HttpGet("/login/{username}/{password}")]
+    [HttpGet("Login/{username}/{password}")]
     public IActionResult Get([FromRoute] string username, [FromRoute] string password)
     {
         string token = _userService.Login(username, password);
@@ -26,14 +26,67 @@ public class UserController : ControllerBase
         return BadRequest(new { msg = "Invalid username or password" });
     }
 
-    [HttpPost("/register")]
+    [HttpPost("Register")]
     public async Task<IActionResult> Post([FromBody] User user)
     {
+        System.Console.WriteLine($"Usuario disponible: {_userService.UsernameAvailable(user.Username)}");
+        System.Console.WriteLine($"Mail disponible: {_userService.EmailAvailable(user.Email)}");
         if (_userService.UsernameAvailable(user.Username) && _userService.EmailAvailable(user.Email))
         {
             await _userService.Register(user);
             return Ok(new { msg = "User registered" });
         }
         return BadRequest(new { msg = "Username or email already in use" });
+    }
+
+    [HttpPut("Edit/Name/{id}/{name}")]
+    public async Task<IActionResult> PutName([FromRoute] Guid id, [FromRoute] string name)
+    {
+        if (await _userService.EditName(name, id))
+        {
+            return Ok(new { msg = "Name edited" });
+        }
+        return BadRequest(new { msg = "User not found" });
+    }
+
+    [HttpPut("Edit/Username/{id}/{username}")]
+    public async Task<IActionResult> PutUsername([FromRoute] Guid id, [FromRoute] string username)
+    {
+        if (await _userService.EditUsername(username, id))
+        {
+            return Ok(new { msg = "Username edited" });
+        }
+        return BadRequest(new { msg = "User not found" });
+    }
+
+    [HttpPut("Edit/Password/{id}/{password}")]
+    public async Task<IActionResult> PutPassword([FromRoute] Guid id, [FromRoute] string password)
+    {
+        if (await _userService.EditPassword(password, id))
+        {
+            return Ok(new { msg = "Password edited" });
+        }
+        return BadRequest(new { msg = "User not found" });
+    }
+
+    [HttpPut("Edit/Email/{id}/{email}")]
+    public async Task<IActionResult> PutEmail([FromRoute] Guid id, [FromRoute] string email)
+    {
+        if (await _userService.EditEmail(email, id))
+        {
+            return Ok(new { msg = "Email edited" });
+        }
+        return BadRequest(new { msg = "User not found" });
+    }
+
+    [HttpDelete("Delete/{id}")]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+
+        if (await _userService.Delete(id))
+        {
+            return Ok(new { msg = "User deleted" });
+        }
+        return BadRequest(new { msg = "User not found" });
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -7,8 +7,11 @@ public interface IUserService
 {
     string Login(string username, string password);
     Task Register(User user);
-    Task Edit(User user, Guid id);
-    Task Delete(Guid id);
+    Task<bool> EditName(string name, Guid id);
+    Task<bool> EditUsername(string username, Guid id);
+    Task<bool> EditPassword(string password, Guid id);
+    Task<bool> EditEmail(string email, Guid id);
+    Task<bool> Delete(Guid id);
     bool UsernameAvailable(string username);
     bool EmailAvailable(string email);
 }
@@ -22,38 +25,75 @@ public class UserService : IUserService
         _context = context;
     }
 
-    public async Task Delete(Guid id)
+    public async Task<bool> Delete(Guid id)
     {
         User user = await _context.Users.FindAsync(id);
-        if (user != null)
+        if (user != null && user.Status)
         {
             user.Status = false;
             await _context.SaveChangesAsync();
+            return true;
         }
+        return false;
     }
 
-    public async Task Edit(User editedUser, Guid id)
+    public async Task<bool> EditEmail(string email, Guid id)
     {
         User user = await _context.Users.FindAsync(id);
-        if (user != null)
+        if (user != null && user.Status)
         {
-            user.Name = editedUser.Name;
-            user.Email = editedUser.Email;
-            user.Username = editedUser.Username;
-            user.Password = Encrypt.GetSHA512(editedUser.Password);
+            user.Email = email;
             await _context.SaveChangesAsync();
+            return true;
         }
+        return false;
+    }
+
+    public async Task<bool> EditName(string name, Guid id)
+    {
+        User user = await _context.Users.FindAsync(id);
+        if (user != null && user.Status)
+        {
+            user.Name = name;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        return false;
+    }
+
+    public async Task<bool> EditPassword(string password, Guid id)
+    {
+        User user = await _context.Users.FindAsync(id);
+        if (user != null && user.Status)
+        {
+            user.Password = Encrypt.GetSHA512(password);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        return false;
+    }
+
+    public async Task<bool> EditUsername(string username, Guid id)
+    {
+        User user = await _context.Users.FindAsync(id);
+        if (user != null && user.Status)
+        {
+            user.Username = username;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        return false;
     }
 
     public bool EmailAvailable(string email)
     {
-        return _context.Users.Any(u => u.Email == email);
+        return !_context.Users.Any(u => u.Email == email);
     }
 
     public string Login(string username, string password)
     {
         User user = _context.Users.FirstOrDefault(u => u.Username == username && u.Password == Encrypt.GetSHA512(password));
-        if (user != null)
+        if (user != null && user.Status)
         {
             return user.Id.ToString();
         }
@@ -71,6 +111,6 @@ public class UserService : IUserService
 
     public bool UsernameAvailable(string username)
     {
-        return _context.Users.Any(u => u.Username == username);
+        return !_context.Users.Any(u => u.Username == username);
     }
 }
